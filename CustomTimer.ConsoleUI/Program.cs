da801452e109cd82ac67b5273ec650fe3e7aa5b8
IvanPuchkov <ivan.puchkov1995@gmail.com>
2015-11-05 11:40:55 +0300

    Task1

---
+++ CustomTimer.ConsoleUI/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CustomTimer.ConsoleUI
+{
+    class MicrowaveAlarm
+    {
+        public MicrowaveAlarm(CustomTimer timer)
+        {
+            if (timer == null)
+                throw new ArgumentNullException();
+            Console.WriteLine("MicrowaveAlarm is now registered");
+            timer.TimeIsUp += MicrowaveAlarmMsg;
+        }
+
+        public void Register(CustomTimer timer)
+        {
+            if (timer == null)
+                throw new ArgumentNullException();
+            Console.WriteLine("MicrowaveAlarm is now registered");
+            timer.TimeIsUp += MicrowaveAlarmMsg;
+        }
+
+        public void Unregister(CustomTimer timer)
+        {
+            if (timer == null)
+                throw new ArgumentNullException();
+            Console.WriteLine("Microwave is now unregistered");
+            timer.TimeIsUp -= MicrowaveAlarmMsg;
+        }
+
+        public void MicrowaveAlarmMsg(object sender, EventArgs eventArgs)
+        {
+            Console.WriteLine("Food is ready");
+        }
+    }
+
+    class HandWatchAlarm
+    {
+        public HandWatchAlarm(CustomTimer timer)
+        {
+            if (timer==null)
+                throw new ArgumentNullException();
+            Console.WriteLine("HandWatchAlarm is now registered");
+            timer.TimeIsUp += AlarmMsg;
+        }
+
+        public void Register(CustomTimer timer)
+        {
+            if (timer == null)
+                throw new ArgumentNullException();
+            Console.WriteLine("HandWatchAlarm is now registered");
+            timer.TimeIsUp += AlarmMsg;
+        }
+
+        public void Unregister(CustomTimer timer)
+        {
+            if (timer==null)
+                throw new ArgumentNullException();
+            Console.WriteLine("HandWatchAlarm is now unregistered");
+            timer.TimeIsUp -= AlarmMsg;
+        }
+
+        public void AlarmMsg(object sender, EventArgs eventArgs)
+        {
+            Console.WriteLine("HandWatch alarm is ringing");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomTimer customTimer=new CustomTimer();
+            MicrowaveAlarm microwaveAlarm=new MicrowaveAlarm(customTimer);
+            HandWatchAlarm handWatchAlarm=new HandWatchAlarm(customTimer);
+            customTimer.StartTimer(5);
+            Thread.Sleep(5500);
+            microwaveAlarm.Unregister(customTimer);
+            customTimer.StartTimer(3);
+            Thread.Sleep(3500);
+            handWatchAlarm.Unregister(customTimer);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CustomTimer/CustomTimer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Timers;
+
+namespace CustomTimer
+{
+    public class CustomTimer
+    {
+        private bool _alreadyStarted = false;
+        private readonly object _locker=new object();
+        private readonly Timer _timer;
+
+        public delegate void TimeIsUpEventHandler(object sender, EventArgs e);
+
+        public event TimeIsUpEventHandler TimeIsUp;
+
+        public CustomTimer()
+        {
+            _timer=new Timer();
+            _timer.Elapsed += OnTimeIsUp;
+            _timer.AutoReset = false;
+        }
+
+        public virtual void OnTimeIsUp(object sender, EventArgs e)
+        {
+            if(TimeIsUp!=null)
+                TimeIsUp(sender, e);
+            lock(_locker)
+            {
+                _alreadyStarted = false;
+            }
+        }
+
+        public void StartTimer(int seconds)
+        {
+            if (!_alreadyStarted)
+            {
+                lock (_locker)
+                {
+                    if (!_alreadyStarted)
+                    {
+                        _timer.Interval = 1000*seconds;
+                        _timer.Enabled=true;
+                        _alreadyStarted = true;
+                    }
+                }
+            }
+        }
+    }
+}
